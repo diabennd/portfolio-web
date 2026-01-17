f5e8d46c47bbe50b1fe994a941a1f4d20b02df2f
diabennd <aidulbeni@gmail.com>
2026-01-18 00:34:53 +0700

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <>
+            <div className="header-section">
+                <a href="">Home</a>
+                <a href="">Projects</a>
+                <a href="">Contact</a>
+                <a href="">Articles</a>
+            </div>
+        </>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+const Profile = () => {
+  return (
+    <>
+      <div className="profile-section">
+        <div className="profile-img"></div>
+        <div className="profile-desc">
+          <h1>Hi There, I'am Benny Way</h1>
+        </div>
+        <div className="profile-social"></div>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
